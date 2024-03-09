be29a4799377f64ec8ef4ff689eb43e296cb5db8
tom <aa123593465@gmail.com>
2024-03-09 14:30:08 +0800

    fix: k6 test function

--- k6/loadtest.js
+++ k6/loadtest.js
@@ -2,6 +2,25 @@ import http from 'k6/http';
 import {check, sleep} from 'k6';
 
 
+function randomInt(min, max) {
+    // return [min, max] 
+    let numA =  Math.floor(Math.random() * (max - min + 1) + min);
+    let numB =  Math.floor(Math.random() * (max - min + 1) + min);
+    let sortNum = [numA, numB].sort((a, b) => a - b);
+    return sortNum;
+}
+
+function randomGender() {
+    return Math.random() < 0.5 ? 'M' : 'F';
+}
+
+function randomCountry() {
+    const countries = ['TW', 'US', 'JP'];
+    return countries[Math.floor(Math.random() * countries.length)]
+}
+
+
+
 export let options = {
     stages:[
        
@@ -17,7 +36,10 @@ export let options = {
 
 
 export default function () {
-    let res = http.get('http://localhost:8080/ads?age_start=18&age_end=80&country[]=US&platform[]=ios&gender[]=F&limit=10&offset=0');
+    let [age_start, age_end] = randomInt(18, 80);
+    let country = randomCountry();
+    let gender = randomGender();
+    let res = http.get(`http://localhost:8080/ads?age_start=${age_start}&age_end=${age_end}&country[]=${country}&platform[]=ios&gender[]=${gender}&limit=10&offset=0`);
     check(res, {'status was 200': (r) => r.status == 200});
     sleep(1);
 }
